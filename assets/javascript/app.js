315e7614b56bb99667f6851ed3eddcae5944cea1
mradke2 <mrbunited@gmail.com>
2018-07-13 12:43:24 -0500

    css added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,58 @@
 $(document).ready(function () {
 
-var config = {
-    apiKey: "AIzaSyAwDSUQlMyO0I7hIATkwhiuFIu0D7GuZLM",
-    authDomain: "train-schedule-72cb0.firebaseapp.com",
-    databaseURL: "https://train-schedule-72cb0.firebaseio.com",
-    projectId: "train-schedule-72cb0",
-    storageBucket: "",
-    messagingSenderId: "811477280227"
-};
+    var config = {
+        apiKey: "AIzaSyAwDSUQlMyO0I7hIATkwhiuFIu0D7GuZLM",
+        authDomain: "train-schedule-72cb0.firebaseapp.com",
+        databaseURL: "https://train-schedule-72cb0.firebaseio.com",
+        projectId: "train-schedule-72cb0",
+        storageBucket: "",
+        messagingSenderId: "811477280227"
+    };
 
-firebase.initializeApp(config);
-var database = firebase.database();
-console.log(database);
-
-
-var nameVar = "";
-var destVar = "";
-var startVar = 0;
-var freqVar = 0;
-var nextVar = 0;
-var awayVar = 0;
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    console.log(database);
 
 
+    var nameVar = "";
+    var destVar = "";
+    var startVar = 0;
+    var freqVar = 0;
+    var nextVar = 0;
+    var awayVar = 0;
 
-$("#run-submit").on("click", function (event) {
 
-    event.preventDefault();
 
-    // Get inputs
-    nameVar = $("#submit-name").val().trim();
-    destVar = $("#submit-dest").val().trim();
-    startVar = $("#submit-first").val().trim();
-    freqVar = $("#submit-freq").val().trim();
-    var freqInt = parseInt(freqVar);
+    $("#run-submit").on("click", function (event) {
 
-    var valid = (startVar.search(/^\d{2}\d{2}$/) != -1) &&
-    (startVar.substr(0,2) >= 0 && startVar.substr(0,2) <= 24) &&
-    (startVar.substr(3,2) >= 0 && startVar.substr(3,2) <= 59) ;
-console.log(valid);
-console.log(freqInt);
-if (valid == false || freqInt < 0 || freqVar.length <= 0){
-    alert("Wrong input");
-}
-else {
-    // Change what is saved in firebase, pattern - same names for data path in database and 
-    database.ref().push({
-        nameVar: nameVar,
-        destVar: destVar,
-        freqVar: freqVar,
-        startVar: startVar,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        event.preventDefault();
+
+        // Get inputs
+        nameVar = $("#submit-name").val().trim();
+        destVar = $("#submit-dest").val().trim();
+        startVar = $("#submit-first").val().trim();
+        freqVar = $("#submit-freq").val().trim();
+        var freqInt = parseInt(freqVar);
+
+        var valid = (startVar.search(/^\d{2}\d{2}$/) != -1) &&
+            (startVar.substr(0, 2) >= 0 && startVar.substr(0, 2) <= 24) &&
+            (startVar.substr(3, 2) >= 0 && startVar.substr(3, 2) <= 59);
+        console.log(valid);
+        console.log(freqInt);
+        if (valid == false || freqInt < 0 || freqVar.length <= 0) {
+            alert("Wrong input");
+        }
+        else {
+            // Change what is saved in firebase, pattern - same names for data path in database and 
+            database.ref().push({
+                nameVar: nameVar,
+                destVar: destVar,
+                freqVar: freqVar,
+                startVar: startVar,
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
+            });
+        }
     });
-}
-});
 
     database.ref().on("child_added", function (snapshot) {
 
@@ -73,9 +73,9 @@ else {
         console.log("MINUTES TILL TRAIN: " + awayVar);
         var nextVar = moment().add(awayVar, "minutes").format("HH:mm");
         console.log("ARRIVAL TIME: " + moment(nextVar));
-//   display in new row
-        var nextRow = "<tr><td>" + (snapshot.val().nameVar) + "</td><td>" + (snapshot.val().destVar) + "</td><td>"  + (snapshot.val().freqVar) + "</td><td>" + nextVar + "</td><td>" + awayVar + "</td></tr>";
-        
+        //   display in new row
+        var nextRow = "<tr><td>" + (snapshot.val().nameVar) + "</td><td>" + (snapshot.val().destVar) + "</td><td>" + (snapshot.val().freqVar) + "</td><td>" + nextVar + "</td><td>" + awayVar + "</td></tr>";
+
         $("table tbody").prepend(nextRow);
 
 
@@ -83,12 +83,12 @@ else {
     });
 
 
-$('#clock').fitText(1.3);
+    $('#clock').fitText(1.3);
 
     function update() {
-      $('#clock').html(moment().format('MMMM Do YYYY, HH:mm:ss'));
+        $('#clock').html(moment().format('MMMM Do YYYY, HH:mm:ss'));
     }
-    
+
     setInterval(update, 1000);
 
 });
