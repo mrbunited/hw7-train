8958235208cb79c0b1230a056e22e83c5e35c256
mradke2 <mrbunited@gmail.com>
2018-07-13 10:21:36 -0500

    validation added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,7 +32,17 @@ $("#run-submit").on("click", function (event) {
     destVar = $("#submit-dest").val().trim();
     startVar = $("#submit-first").val().trim();
     freqVar = $("#submit-freq").val().trim();
+    var freqInt = parseInt(freqVar);
 
+    var valid = (startVar.search(/^\d{2}\d{2}$/) != -1) &&
+    (startVar.substr(0,2) >= 0 && startVar.substr(0,2) <= 24) &&
+    (startVar.substr(3,2) >= 0 && startVar.substr(3,2) <= 59) ;
+console.log(valid);
+console.log(freqInt);
+if (valid == false || freqInt < 0 || freqVar.length <= 0){
+    alert("Wrong input");
+}
+else {
     // Change what is saved in firebase, pattern - same names for data path in database and 
     database.ref().push({
         nameVar: nameVar,
@@ -41,7 +51,9 @@ $("#run-submit").on("click", function (event) {
         startVar: startVar,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
+}
 });
+
     database.ref().on("child_added", function (snapshot) {
 
         // moment.js calcualtions 
